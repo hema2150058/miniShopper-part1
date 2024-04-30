547648c88f0551a6b8ebae488dbd96633bed9c49
hema2150058 <hemapriyavarkala@gmail.com>
2024-05-01 00:59:13 +0530

    missed while saving

--- order-service/src/main/java/com/mini/shopper/repo/BillingRepo.java
+++ order-service/src/main/java/com/mini/shopper/repo/BillingRepo.java
@@ -1,5 +1,11 @@
 package com.mini.shopper.repo;
 
-public interface BillingRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mini.shopper.model.Billing;
+
+@Repository
+public interface BillingRepo extends JpaRepository<Billing, Long> {
 
 }
--- order-service/src/main/java/com/mini/shopper/service/OrderService.java
+++ order-service/src/main/java/com/mini/shopper/service/OrderService.java
@@ -1,5 +1,27 @@
 package com.mini.shopper.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mini.shopper.repo.BillingRepo;
+import com.mini.shopper.repo.CartRepo;
+import com.mini.shopper.repo.OrderRepo;
+import com.mini.shopper.repo.OrderedProductRepo;
+
+@Service
 public class OrderService {
 
+	@Autowired
+	CartRepo cartRepo;
+	
+	@Autowired
+	OrderedProductRepo orderProductRepo;
+	
+	@Autowired
+	BillingRepo billingRepo;
+	
+	@Autowired
+	OrderRepo orderRepo;
+	
+	
 }
